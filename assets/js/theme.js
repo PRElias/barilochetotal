488242c63592290316e5ea0599a33824794982fe
Paulo Roberto Elias <ppuspfc@gmail.com>
2021-02-26 18:58:40 -0300

    pwa

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -182,3 +182,31 @@ $(document).ready(function(){
 
 
 });
+
+
+// PWA
+
+if ('serviceWorker' in navigator) {
+	//Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
+	if (navigator.serviceWorker.controller) {
+	  console.log('[PWA Builder] active service worker found, no need to register')
+	} else {
+	  //Register the ServiceWorker
+	  window.addEventListener('load', function () {
+		navigator.serviceWorker.register('/pwabuilder-sw.js', {
+		  scope: './'
+		}).then(function (reg) {
+		  console.log('Service worker has been registered for scope:' + reg.scope);
+		});
+	  });
+	  var worker = new Worker('/pwabuilder-sw.js');
+  
+	  worker.addEventListener('message', function (e) {
+		console.log('Worker said: ', e.data);
+	  }, false);
+  
+	  worker.postMessage('Worker doing work!');
+	}
+  } else {
+	console.log("Navegador não aceita serviceWorker");
+  }
